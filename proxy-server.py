07add94ae7b2d247d4ec2bd4c5fc8bea61f322f6
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-08-12 13:22:03 +0500

    Update proxy-server.py

--- proxy-server.py
+++ proxy-server.py
@@ -1,3 +1,9 @@
 import urllib
 
-URL = input("Enter the URL: ")
+URL = input("Enter the URL you want to have anonymous access to: ")
+
+PROXY_ADDRESS = input("Enter proxy address: ")
+
+if __name__ == __main__:
+  
+  pass
